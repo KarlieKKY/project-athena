0dfa9e4ccb1d01f0aea266c87306ab670cd92853
KarlieKKY <yanndeel@gmail.com>
2026-01-06 20:34:07 +0000

    Delete extra files

--- frontend/src/components/common/Spinner.tsx
+++
@@ -1,7 +0,0 @@
-const Spinner = () => {
-  return (
-    <div className="inline-block w-8 h-8 border-4 border-purple-500 border-t-transparent rounded-full animate-spin" />
-  );
-};
-
-export default Spinner;
